dd42fe90ef9396e1305d25e058e8fca8040adaa7
kidd0x <46250487+kidd0x@users.noreply.github.com>
2025-02-13 05:30:44 +0200

    Update main.lua

--- instaentkill/main.lua
+++ instaentkill/main.lua
@@ -4,6 +4,9 @@ local game = Game()
 local function OnEntityTakeDamage(_, entity, amount, damageFlags, damageSource, damageCountdownFrames)
     if entity.Type == EntityType.ENTITY_POOP or entity.Type == EntityType.ENTITY_FIREPLACE then
         if damageSource.Type == EntityType.ENTITY_TEAR then
+            if entity.Type == EntityType.ENTITY_FIREPLACE and entity.Variant == 1 then
+                return
+            end
             entity:Die()
         end
     end
