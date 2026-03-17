46632e9e2c900c83a5948fd446bf76835ed27346
kidd0x <46250487+kidd0x@users.noreply.github.com>
2026-03-17 15:06:52 +0200

    Adaugat font

--- crawlspace_detector/main.lua
+++ crawlspace_detector/main.lua
@@ -1,16 +1,29 @@
 local mod = RegisterMod("CrawlSpaceDetector", 1)
-
+ 
 local hasCrawlSpace = false
-
+ 
+-- Fonturi disponibile in joc:
+-- "font/terminus.fnt"       
+-- "font/pftempestaseven.fnt"
+-- "font/upheaval.fnt"       
+-- "font/teammeatfont10.fnt" 
+-- https://wofsauge.github.io/IsaacDocs/rep/tutorials/Tutorial-Rendertext.html?h=rendertext#example-code
+local font = Font()
+font:Load("font/teammeatfont10.fnt")
+ 
 function mod:OnNewRoom()
     local room = Game():GetRoom()
     hasCrawlSpace = room:GetDungeonRockIdx() ~= -1
 end
 mod:AddCallback(ModCallbacks.MC_POST_NEW_ROOM, mod.OnNewRoom)
-
+ 
 function mod:OnRender()
     if not hasCrawlSpace then return end
-    -- RenderText("String", x, y, r, g, b, a) daca vreti sa modificati ceva
-    Isaac.RenderText("CRAWL SPACE", 208, 60, 255, 0, 0, 1)
+    -- DrawString(str, x, y, KColor, boxWidth, center)
+    -- KColor(r, g, b, a) - valori 0-1
+    -- boxWidth 0 = no wrap
+    -- center true = fixat pe x
+    font:DrawString("CRAWL SPACE", 200, 60, KColor(1, 0, 0, 1), 0, false)
 end
 mod:AddCallback(ModCallbacks.MC_POST_RENDER, mod.OnRender)
+ 
